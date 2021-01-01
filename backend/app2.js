e3cc92d80c0add0fd9497d9917b000c40417c344
praveenalpha <pm495949@gmail.com>
2021-01-01 05:37:01 +0530

    mailtrap sent

--- backend/app2.js
+++ backend/app2.js
@@ -1,2 +1,36 @@
+const express = require('express');
 const nodemailer = require("nodemailer");
+const app = express();
+const port = 3000;
 
+
+async function sendEmail() {
+    try{
+        const transporter = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "533f6b192b7799",
+              pass: "0568fbfa79e7e5"
+            }
+        });
+        
+          let info = await transporter.sendMail({
+          from: 'pm495949@gmail.com', // sender address
+          to: "bar@example.com, baz@example.com", // list of receivers
+          subject: "Hello ✔", // Subject line
+          text: "Hello world?", // plain text body
+          html: "<b>Hello world?</b>", // html body
+        });
+
+    }
+    catch(error){
+
+    }
+}
+
+sendEmail();
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  });
