ab2e3248b1f64a0e730a01b3f7c6d0016fb153a6
Praveen Mani <48654802+praveenalpha@users.noreply.github.com>
2021-03-06 21:41:18 +0530

    Delete backend directory

--- backend/router/paymentRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const paymentRouter = express.Router();
-const {createPaymentSession} = require('../controller/paymentController');
-
-
-paymentRouter.route("").post(createPaymentSession);
-
-
-module.exports = paymentRouter;
--- backend/view/script/header-script.js
+++
@@ -1,7 +0,0 @@
-let signup = document.querySelector(".signup");
-
-signup.addEventListener("click", (e) =>{
-    e.preventDefault();
-    console.log(window.location);
-    window.location.href = "/foodApp/backend/view/signup.html";
-})
